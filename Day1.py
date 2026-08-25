20385e7d312251ed1b4ec50d54d3a4aeaf21155a
mekams <kamraneg@gmail.com>
2026-08-25 01:03:50 +0100

    learning fundamentals

---
+++ Day1.py
@@ -0,0 +1,11 @@
+#defining a varibale
+"""snake_case_variable = "This is a snake case variable"
+print(snake_case_variable)"""
+
+a = "COLLEGE"
+print(a)
+
+print(a[3:6])  # This will print characters from index 3 to 5 (6 is not included)
+print(a[3:6:2])  # This will print characters from index 3 to 5 with a step of 2
+print(a[0:7:2])  # This will print characters from index 0 to 6 with a step of 2
+print(a[::2])  # This will print every second character from the entire string  
