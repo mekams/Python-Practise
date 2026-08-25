043974ffc6c9939ccb265bc6170e24531d48d8d4
mekams <kamraneg@gmail.com>
2026-08-25 02:37:16 +0100

    learning fundamentals

--- Day1.py
+++ Day1.py
@@ -2,10 +2,25 @@
 """snake_case_variable = "This is a snake case variable"
 print(snake_case_variable)"""
 
-a = "COLLEGE"
+'''a = "COLLEGE"
 print(a)
 
 print(a[3:6])  # This will print characters from index 3 to 5 (6 is not included)
 print(a[3:6:2])  # This will print characters from index 3 to 5 with a step of 2
 print(a[0:7:2])  # This will print characters from index 0 to 6 with a step of 2
 print(a[::2])  # This will print every second character from the entire string  
+
+a = "Hello how are you?"
+
+print(a[6:9:1],"\n", a[14:17:1],"\n", a[0:5:1])  # This will print "how you Hello"'''
+
+#limitations
+# a = '12.2'
+# a =float(a)
+# a=int(a)
+# print(a)
+
+age = int(input("Enter your age: "))
+
+print("Your age is:", age)
+print(f"Your age is: {age}")
